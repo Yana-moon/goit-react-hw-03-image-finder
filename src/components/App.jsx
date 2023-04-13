b5eb183dbe53b9972bfac717d3d9cf9dea744080
Яна <112972800+Yana-moon@users.noreply.github.com>
2023-04-13 08:14:53 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -19,7 +19,7 @@ export class App extends Component {
     isLoading: false,
   };
 
-  async componentDidUpdate(prevProps, prevState) {
+  async componentDidUpdate(_prevProps, prevState) {
     const options = {
       searchName: this.state.searchName,
       currentPage: this.state.currentPage,
